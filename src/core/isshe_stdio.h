db27da9f139333041cc1678aa5625b85c2b2f1f8
i.sshe <i.sshe@foxmail.com>
2019-12-28 12:33:52 +0800

    20191228: 删除多余的.h文件

--- src/core/isshe_stdio.h
+++
@@ -1,8 +0,0 @@
-#ifndef _ISSHE_STDIO_H_
-#define _ISSHE_STDIO_H_
-
-char *isshe_fgets(char *ptr, int n, FILE *stream);
-
-void isshe_fputs(const char *ptr, FILE *stream);
-
-#endif
--- src/core/isshe_time.h
+++
@@ -1,8 +0,0 @@
-#ifndef _ISSHE_TIME_H_
-#define _ISSHE_TIME_H_
-
-int isshe_gettimeofday(struct timeval *tv, struct timezone *tz);
-
-char * isshe_gf_time(void);
-
-#endif
